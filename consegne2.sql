1184a21a5fee3aebc5413f3cb9e16ac793de2800
Filippo Montani <78483736+Carnicero90@users.noreply.github.com>
2021-05-26 15:07:20 +0200

    Add files via upload

---
+++ consegne2.sql
@@ -0,0 +1,33 @@
+/* 
+  1. Conta gli ospiti raggruppandoli per anno di nascita
+*/
+SELECT YEAR(`date_of_birth`) AS `yob`, COUNT('id') AS `tot`
+FROM `ospiti`
+GROUP BY YEAR(`date_of_birth`);
+/*      
+  2. Somma i prezzi dei pagamenti raggruppandoli per status 
+*/
+SELECT `status`, SUM(`price`) AS `total`
+FROM `pagamenti`
+GROUP BY `status`;
+/*      
+  3. Conta quante volte è stata prenotata ogni stanza 
+*/
+SELECT `stanza_id` AS `stanza`, COUNT(*) AS `total`
+FROM `prenotazioni`
+GROUP BY `stanza`;
+/*      
+  4.       
+*/
+SELECT HOUR(`created_at`) AS `ora_prenotazione`, COUNT(HOUR(`created_at`)) AS `prenotazioni_totali`
+FROM `prenotazioni`
+GROUP BY `ora_prenotazione`
+ORDER BY `prenotazioni_totali` DESC;
+/*      
+  5.       
+*/
+SELECT `ospite_id`, COUNT(*) AS `prenotazioni_totali`
+FROM `prenotazioni_has_ospiti`
+GROUP BY `ospite_id`
+ORDER BY `prenotazioni_totali` DESC
+LIMIT 1;
