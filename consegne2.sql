581407da1199f4797afc9029cd43629d4e2e44ea
Filippo Montani <78483736+Carnicero90@users.noreply.github.com>
2021-05-26 16:24:23 +0200

    aggiunte consegne di seconda e terza query in consegne2.sql

--- consegne2.sql
+++ consegne2.sql
@@ -17,14 +17,14 @@ SELECT `stanza_id` AS `stanza`, COUNT(*) AS `total`
 FROM `prenotazioni`
 GROUP BY `stanza`;
 /*      
-  4.       
+  4. Fai un'analisi per vedere se ci sono ore in cui le prenotazioni sono più frequenti
 */
 SELECT HOUR(`created_at`) AS `ora_prenotazione`, COUNT(HOUR(`created_at`)) AS `prenotazioni_totali`
 FROM `prenotazioni`
 GROUP BY `ora_prenotazione`
 ORDER BY `prenotazioni_totali` DESC;
 /*      
-  5.       
+  5. Quante prenotazioni ha fatto l'ospite che ha fatto più prenotazioni? (Quante, non chi!) 
 */
 SELECT `ospite_id`, COUNT(*) AS `prenotazioni_totali`
 FROM `prenotazioni_has_ospiti`
